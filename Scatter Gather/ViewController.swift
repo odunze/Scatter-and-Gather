1075a4a7244868c592eca036ea24d29c4db2cf96
Lotanna <sugabelly@gmail.com>
2019-01-10 17:29:28 -0500

    animated labels

--- Scatter Gather/ViewController.swift
+++ Scatter Gather/ViewController.swift
@@ -15,12 +15,60 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    let shouldScramble: Bool?
+    var shouldScramble: Bool = false
 
-    @IBAction func toggle(_ sender: UIBarButtonItem) {
+
+    @IBAction func toggle(_ Sender: Any) {
+        let viewArray: [UIView] = [L, O, T, A, N, N2, A2]
+        let duration: Double = 2.0
+
+
         
+        //Toggle
+        if shouldScramble == false {
         
+            shouldScramble = true } else { shouldScramble = false }
+        
+        //Animation
+        
+        if shouldScramble == true {
+            
+            for x in viewArray {
+           
+            UIView.animate(withDuration: duration, animations: {
+                
+                self.moveRight(view: x) })//Note the animations and its functions are now inside the parentheses.
+            { //This starts the completion part
+                
+                (finished) in
+                if finished { UIView.animate(withDuration: duration, animations: {
+                    
+                    self.moveLeft(view: x) }) } //Move back to the left after moving right
+                
+                }//This ends the completion part.
+            } //End of for loop
+        }
     }
+    
+    func moveRight(view: UIView) {
+        view.center.x += 300 //Increments the X position of the view to move it to the right.
+    }
+    
+    func moveLeft(view: UIView) {
+        view.center.x -= 300 //Decrements the X position of the view to move it to the left.
+    }
+    
+    
+    //Outlets
+    
+    @IBOutlet weak var L: UILabel!
+    @IBOutlet weak var O: UILabel!
+    @IBOutlet weak var T: UILabel!
+    @IBOutlet weak var A: UILabel!
+    @IBOutlet weak var N: UILabel!
+    @IBOutlet weak var N2: UILabel!
+    @IBOutlet weak var A2: UILabel!
+
     
 }
 
